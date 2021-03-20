1ae335a63fbe61ce51c3248dec86f0049dae1037
Luis A. Zapata Icart <expansion88@pm.me>
2021-03-19 22:46:13 -0300

    Folders clean up and Firebase initialization

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCXWh2dTvbQmZsW9uAu2Pf1Pt6qJhJZxg4",
+  authDomain: "netflix-clone-eab03.firebaseapp.com",
+  projectId: "netflix-clone-eab03",
+  storageBucket: "netflix-clone-eab03.appspot.com",
+  messagingSenderId: "992021299057",
+  appId: "1:992021299057:web:38836dba5770b5c73740d0"
+}
