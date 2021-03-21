c5e9fb0c716097abc6c54a1d05faaac20aeff35f
Luis A. Zapata Icart <expansion88@pm.me>
2021-03-21 20:35:01 -0300

    Banner Created
    Banner has been created + functionality to truncate the descriptions if they are too long

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import NavBar from '../../components/NavBar/index'
+import Banner from '../../components/Banner/index'
 
 function HomeScreen() {
   return (
     <div className='homeScreen'>
       <NavBar />
+      <Banner />
     </div>
   )
 }
