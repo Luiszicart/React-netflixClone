2c850e249bec6180461cc7888f82e4bf7354f69a
Luis A. Zapata Icart <expansion88@pm.me>
2021-03-25 22:35:30 -0300

    Auth implementation

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyCXWh2dTvbQmZsW9uAu2Pf1Pt6qJhJZxg4",
   authDomain: "netflix-clone-eab03.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "992021299057",
   appId: "1:992021299057:web:38836dba5770b5c73740d0"
 }
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth }
+export default db
