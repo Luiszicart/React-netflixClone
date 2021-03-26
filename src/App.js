587f7443b0d38cabbcf982dce94753d823b06703
Luis A. Zapata Icart <expansion88@pm.me>
2021-03-26 17:00:25 -0300

    Redux implemented
    State saved between multiples components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import './App.css';
 import { auth } from './firebase';
 import HomeScreen from './screens/HomeScreen/index'
 import LoginScreen from './screens/Login/index';
+import { login, logout, selectUser } from './features/userSlice'
 
 function App() {
-  const user = null
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        
-      } else {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+        })
+      )
+    } else {
+      dispatch(logout)
+    }
+  })
 
-      }
-    })
-
-    return unsubscribe
-  }, [])
+  return unsubscribe
+}, [])
 
   return (
     <div className="app">
