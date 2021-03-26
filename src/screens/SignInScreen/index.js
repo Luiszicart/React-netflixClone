25565affe1fd08027f5b654545d535817bccdb6d
Luis A. Zapata Icart <expansion88@pm.me>
2021-03-25 22:01:02 -0300

    SignIn screen implementation

--- src/screens/SignInScreen/index.js
+++ src/screens/SignInScreen/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import './styles.css'
+
 function SignInScreen() {
+
+  const register = (e) => {
+    e.preventDefault()
+  }
+
+  const signIn = (e) => {
+    e.preventDefault()
+  }
+  
   return (
-    <div>
-      <h1>Test</h1>
+    <div className='signUpScreen'>
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder='Email' type='email'></input>
+        <input placeholder='Password' type='password'></input>
+        <button type='submit' onClick={signIn}>Sign In</button>
+        <h4>
+          <span className='signUpScreen_grey'>New to Netflix?</span>
+          <span className='signUpScreen_link' onClick={register}>Sign Up Now.</span>
+        </h4>
+      </form>
     </div>
   )
 }
