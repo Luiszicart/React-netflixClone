6a28fc04d028f8a0c535b28444b1f6b4929b5b42
Luis A. Zapata Icart <expansion88@pm.me>
2021-03-28 23:45:35 -0300

    Added profile screen

---
+++ src/screens/ProfileScreen/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.css'
+
+function ProfileScreen() {
+  return (
+    <div className='profileScreen'>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default ProfileScreen
