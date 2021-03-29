bc6df5752e42f23cf7ef36479edaccc4942b1e93
Luis A. Zapata Icart <expansion88@pm.me>
2021-03-29 20:18:17 -0300

    Profile Screen Completed

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../features/userSlice'
+import { auth } from '../../firebase'
+import NavBar from '../HomeScreen/components/NavBar'
 
 import './styles.css'
 
 function ProfileScreen() {
+  const user = useSelector(selectUser)
+
   return (
     <div className='profileScreen'>
-      <h1>Test</h1>
+      <NavBar/>
+      <div className='profile_body'>
+        <h1>Edit profile</h1>
+        <div className='profile_info'>
+          <img src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png' alt='Profile' />
+          <div className='profile_details'>
+            <h2>{user.email}</h2>
+            <div className='profile_plans'>
+              <h3>Plans</h3>
+              <p></p>
+              <button
+              onClick={() => auth.signOut()}
+              className='profile_signOut'
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
